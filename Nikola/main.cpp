b80641f71589014d882833200d916855d4066381
Nikola Pavelic <mooncarrot3@gmail.com>
2024-06-05 21:20:28 +0200

    validchannel

--- Nikola/main.cpp
+++ Nikola/main.cpp
@@ -6,56 +6,28 @@
 
 std::vector<std::string> valid_commands;
 
-// void ircMessageParser (std::string &message)
-// {
-// 	std::string prefix;
-// 	std::string command;
-// 	std::vector<std::string> params;
-// 	std::string trailing;
+bool isValidChannelName(std::string channel_name)
+{
+	if (channel_name.size() == 0)
+	{
+		return false;
+	}
+	if (channel_name.size() > 50)
+	{
+		return false;
+	}
+	if (channel_name[0] != '#')
+	{
+		return false;
+	}
+	if (channel_name.find(' ') != std::string::npos) || (channel_name.find(',') != std::string::npos) || (channel_name.find(7) != std::string::npos)
+	{
+		return false;
+	}
+	return true;
+}
 
-// 	if (message[0] == ':')
-// 		prefix = message.substr(1, message.find(' ') - 1);
 
-// 	command = message.substr(0, message.find(' '));
-// 	message = message.substr(message.find(' ') + 1);
-
-// 	while (message[0] == ':')
-// 	{
-// 		params.push_back(message.substr(1, message.find(' ') - 1));
-// 		message = message.substr(message.find(' ') + 1);
-// 	}
-
-// 	while (message.find(' ') != std::string::npos)
-// 	{
-// 		 params.push_back(message.substr(0, message.find(' ')));
-// 		 message = message.substr(message.find(' ') + 1);
-// 	}
-
-// 	trailing = message;
-
-// 	std::cout << "Prefix: " << prefix << std::endl;
-// 	std::cout << "Command: " << command << std::endl;
-// 	for (int i = 0; i < params.size(); i++)
-// 	{
-// 		std::cout << "Param " << i << ": " << params[i] << std::endl;
-// 	}
-// 	std::cout << "Trailing: " << trailing << std::endl;
-
-// 	if (std::find(valid_commands.begin(), valid_commands.end(), command) != valid_commands.end())
-// 	{
-// 		std::cout << "Command is valid" << std::endl;
-// 	}
-// 	else
-// 	{
-// 		std::cout << "Command is invalid" << std::endl;
-// 		return ;
-// 	}
-// 	if (command == "JOIN")
-// 	{
-		
-// 	}
-	
-// }
 
 void ircMessageParser (std::string message)
 {
