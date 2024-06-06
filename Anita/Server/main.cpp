0e6a7df72f5bb1509d7bf0b9510f2f78a16e5b40
Erwin Küchel <e.kuechel>
2024-06-06 18:35:12 +0200

    validPort in main

--- Anita/Server/main.cpp
+++ Anita/Server/main.cpp
@@ -1,6 +1,8 @@
 #include "Server.hpp"
 #include <exception>
 
+bool validPort(int port);
+
 int main(int ac, char **av)
 {
     if (ac != 3)
@@ -8,18 +10,26 @@ int main(int ac, char **av)
         std::cerr << "Usage: ./server [port] [password]" << std::endl;//Currently is the client set to port 4444
         return (1);
     }
-    Server ourServer(atoi(av[1]), av[2]); 
-		try
+
+		if(validPort(atoi(av[1])))
 		{
-			signal(SIGINT, Server::signalHandler);
-			signal(SIGQUIT, Server::signalHandler);
-			ourServer.initServer();
+			Server ourServer(atoi(av[1]), av[2]); 
+			try
+			{
+				signal(SIGINT, Server::signalHandler);
+				signal(SIGQUIT, Server::signalHandler);
+				ourServer.initServer();
+			}
+			catch(const std::exception& e)
+			{
+				//ourServer.closeFds();
+				std::cerr << e.what() << std::endl;
+			}
+			std::cout << "Server destroyed" << std::endl;
 		}
-		catch(const std::exception& e)
-		{
-			//ourServer.closeFds();
-			std::cerr << e.what() << std::endl;
-		}
-		std::cout << "Server destroyed" << std::endl;
-    return (0);
+}
+
+bool validPort(int port)
+{
+	return ((port > 6660 && port < 6669) || port == 7000);
 }
